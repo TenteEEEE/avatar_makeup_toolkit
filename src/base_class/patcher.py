77e61701c38863983c36366e5e5b0639d9247712
TenteEEEE <wander0055@gmail.com>
2020-07-11 21:19:31 +0900

    cleanup patcher class

--- src/base_class/patcher.py
+++ src/base_class/patcher.py
@@ -31,26 +31,25 @@ class patcher():
         else:
             patched = self.base.copy()
         query = self.loader.make_query(index)
-        # if len(query) != 0:
+        if len(query) == 0:
+            return None
         images = self.loader.load_imgs_by_query(query)
         for key in self.converters.keys():
-            if key in self.options.keys(): # Index overwite
-                if self.options[key] < 0:
-                    images[key] = None
-                elif self.options[key] > 0:
+            if key in self.options.keys():
+                if self.options[key] < 0: # Ignore patching
+                    continue
+                elif self.options[key] > 0: # Index overwite
                     images[key] = self.loader.load_img(self.options[key], key)
-            if images[key] is not None:
-                cvts = self.converters[key]
-                if type(cvts) is not list:
-                    cvts = [cvts]
-                for cvt in cvts:
-                    cvt.options = self.options
-                    converted = cvt.get_converted(images[key])
-                    position = [int(converted['position'][i] * self.base_size[i]) for i in range(2)]
-                    if self.mask is None:
-                        patched = improc.overlay(patched, converted['image'], position)
-                    else:
-                        patched = improc.overlay_with_mask(patched, converted['image'], self.mask, position)
+            cvts = self.converters[key]
+            cvts = [cvts] if type(cvts) is not list else cvts
+            for cvt in cvts:
+                cvt.options = self.options
+                converted = cvt.get_converted(images[key])
+                position = [int(converted['position'][i] * self.base_size[i]) for i in range(2)]
+                if self.mask is None:
+                    patched = improc.overlay(patched, converted['image'], position)
+                else:
+                    patched = improc.overlay_with_mask(patched, converted['image'], self.mask, position)
         return patched
 
     def ask(self, question, default=False, default_msg=None):
